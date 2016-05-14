cfc26b08105f7ef9fa41b3a2b75959d0b8e93af2
Britton Baumann <brittonbaumann@gmail.com>
2016-05-14 13:42:09 -0500

    delete completed items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,26 @@ class ItemsController < ApplicationController
       if @item.save
         flash[:notice] = "Item was saved successfully."
         redirect_to current_user
-
       else
         flash.now[:alert] = "There was an error saving the item. Please try again."
+        redirect_to current_user
       end
-      redirect_to current_user
+    end
+
+    def destroy
+      @user = User.find(current_user)
+      @item = Item.find(params[:id])
+
+      if @item.destroy
+        flash[:notice] = "Item was deleted successfully."
+      else
+        flash[:alert] = "Item couldn't be deleted. Please try again."
+      end
+
+      respond_to do |format|
+        format.html
+        format.js
+      end
     end
 
     private
